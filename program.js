9eba9704234d9f7dc15bfc504cec14fdfa5ccd57
Yan-Yi Li <liyanyi@gmail.com>
2016-04-19 21:02:46 +0100

    complete 5/13: Filtered LS

--- program.js
+++ program.js
@@ -1,5 +1,12 @@
-var sum = 0;
-for(var i = 2; i < process.argv.length; i++) {
-  sum += +process.argv[i];
-}
-console.log(sum);
+var fs = require('fs');
+var path = require('path');
+var dir = process.argv[2];
+var filter = process.argv[3];
+
+fs.readdir(dir, function(err, list) {
+  for(var i = 0; i < list.length; i++) {
+    if(path.extname(list[i]) === '.' + filter) {
+      console.log(list[i]);
+    }
+  }
+});
