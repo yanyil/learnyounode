f6a0f2287e5179cf6cba895be3e652ff964d58f3
Yan-Yi Li <liyanyi@gmail.com>
2016-04-20 17:30:05 +0100

    complete 10/13: time server

--- program.js
+++ program.js
@@ -1,28 +1,9 @@
-var http = require('http');
-var bl = require('bl');
-var url = process.argv.slice(2);
-var contents = [];
-var count = 0;
+var strftime = require('strftime');
+var net = require('net');
+var port = process.argv[2];
 
-function httpGet (index) {
-  http.get(url[index], function(response) {
-    response.pipe(bl(function(err, data) {
-      if(err) {
-        return console.error(err);
-      }
-      data = data.toString();
-      contents[index] = data;
-      count++;
+var server = net.createServer(function (socket) {
+  socket.end(strftime('%F %R\n'));
+});
 
-      if(count == 3) {
-        for(var j = 0; j < 3; j++) {
-          console.log(contents[j]);
-        }
-      }
-    }));
-  });
-};
-
-for(var i = 0; i < 3; i++) {
-  httpGet(i);
-}
+server.listen(port);
