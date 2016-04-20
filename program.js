395e6d4a65157ee22b18c10275b2f9023b6c3cd6
Yan-Yi Li <liyanyi@gmail.com>
2016-04-20 11:42:06 +0100

    complete 8/13: http collect

--- program.js
+++ program.js
@@ -1,9 +1,15 @@
 var http = require('http');
 var url = process.argv[2];
+var received = [];
 
 http.get(url, function(response) {
   response.setEncoding("utf8");
   response.on("data", function(data) {
-    console.log(data);
+    received.push(data);
+  });
+  response.on("end", function() {
+    var string = received.join("");
+    console.log(string.length);
+    console.log(string);
   })
 });
