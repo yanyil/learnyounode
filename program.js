5bc05aa5f4a3c2b77493eb68e89bd8794fa3b756
Yan-Yi Li <liyanyi@gmail.com>
2016-04-21 15:31:22 +0100

    complete 12/13: http uppercaserer

--- program.js
+++ program.js
@@ -1,11 +1,11 @@
 var http = require('http');
-var fs = require('fs');
+var map = require('through2-map');
 var port = process.argv[2];
-var file = process.argv[3];
 
 var server = http.createServer(function (req, res) {
-  var stream = fs.createReadStream(file);
-  stream.pipe(res);
+  req.pipe(map(function(chunk) {
+    return chunk.toString().toUpperCase();
+  })).pipe(res)
 });
 
 server.listen(port);
