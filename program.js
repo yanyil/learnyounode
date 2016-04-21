a912eb757d2cd51f4313b2cdcb147013c5c4a527
Yan-Yi Li <liyanyi@gmail.com>
2016-04-21 16:48:40 +0100

    complete 13/13: http json api server

--- program.js
+++ program.js
@@ -1,11 +1,37 @@
 var http = require('http');
-var map = require('through2-map');
+var url = require('url');
 var port = process.argv[2];
 
+function parsetime(time) {
+  return {
+    "hour": time.getHours(),
+    "minute": time.getMinutes(),
+    "second": time.getSeconds()
+  }
+}
+
+function unixtime(time) {
+  return {"unixtime": time.getTime()};
+}
+
 var server = http.createServer(function (req, res) {
-  req.pipe(map(function(chunk) {
-    return chunk.toString().toUpperCase();
-  })).pipe(res)
+  var parsedUrl= url.parse(req.url, true);
+  var time = new Date(parsedUrl.query.iso);
+  var json;
+
+  if(parsedUrl.pathname == "/api/unixtime") {
+    json = unixtime(time);
+  } else if(parsedUrl.pathname == "/api/parsetime") {
+    json = parsetime(time);
+  }
+
+  if(json) {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify(json));
+  } else {
+    res.writeHead(404);
+    res.end();
+  }
 });
 
 server.listen(port);
